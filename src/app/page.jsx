b093430cc19967b7d6645a25754e105a3a04b2d1
cyhammad <hammad727272@gmail.com>
2024-02-14 13:16:55 +0500

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="font-neue-machina-bold font-light text-[100px]">
+        Empowering Your Financial Future HEADading 1
+      </h1>
+      <h1 className="font-neue-machina font-light text-[100px]">
+        Empowering Your Financial Future
+      </h1>
+      <h1 className="font-neue-machina-light font-light text-[100px]">
+        Empowering Your Financial Future
+      </h1>
+      <p className="font-euclid w-96">
+        Experience the seamless convergence of Web 3.0 capabilities with our
+        integrated DApp browser, facilitating staking, crypto transactions,
+        liquidity pool farming, and cross-chain interoperability
+      </p>
+    </main>
+  );
+}
