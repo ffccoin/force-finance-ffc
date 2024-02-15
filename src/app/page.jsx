c32868c9fd858eb3acc4658ef785f313c074ad3f
cyhammad <hammad727272@gmail.com>
2024-02-15 16:57:56 +0500

    homepage progress

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,11 @@
+import Section1 from "@/components/sections/homepage/Section1";
+import Strip from "@/components/sections/homepage/Strip";
+
 export default function Home() {
   return (
-    <div className="flex">
-      dsadd
-      <span></span>
-    </div>
+    <main>
+      <Section1 />
+      <Strip />
+    </main>
   );
 }
---
+++ src/components/sections/homepage/Strip.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Strip = () => {
+  return (
+    <div className="grid h-[120px] w-full place-items-center bg-[url('/homepage/strip.svg')] bg-contain bg-no-repeat"></div>
+  );
+};
+
+export default Strip;
