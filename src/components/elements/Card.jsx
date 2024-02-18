9a23de7fbfe754e9e1af2d9e768704e2af5fbf91
Ramisha22 <96248008+Ramisha22@users.noreply.github.com>
2024-02-19 02:43:13 +0500

    homepage done

--- src/components/elements/Card.jsx
+++ src/components/elements/Card.jsx
@@ -5,7 +5,7 @@ const Card = ({ state, bonusPercent, daysLeft, start, endTime }) => {
   return (
     <div className="card group h-[232px] w-[291px] rounded-3xl bg-neutral hover:h-[242px]">
       <div className="card-content p-3">
-        <h2 className="mt-4  pl-4 ">State: {state}</h2>
+        <h2 className="mt-4  pl-3 ">State: {state}</h2>
         <div className="flex gap-8 justify-between">
           <div>
             <p className="mt-5 pl-4 font-neue-machina-bold text-4xl font-extrabold leading-10 group-hover:text-primary1">
