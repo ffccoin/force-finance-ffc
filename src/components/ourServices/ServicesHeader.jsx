437981435329bad25ab29fbc190093a478289c7d
cyhammad <hammad727272@gmail.com>
2024-02-25 17:26:08 +0500

    services header updated

--- src/components/ourServices/ServicesHeader.jsx
+++ src/components/ourServices/ServicesHeader.jsx
@@ -5,7 +5,7 @@ export default function ServicesHeader() {
   return (
     <div className="mt-14 flex flex-col content-center items-center justify-center">
       <p className="font-neue-machina-bold upercase lg:text-5xl text-[35px] text-center font-[800px] lg:leading-[76px] text-neutralLighter">
-      FFC COIN FINANCIAL SERVICES
+      FFC COIN SERVICES
       </p>
       <div className=" mt-10 md:mx-0 items-center">
       <SecondaryButton title="Lunch Dapp" size="small" color="neutralDarker" className="bg-primary1 border-none text-neutralDarker" />
