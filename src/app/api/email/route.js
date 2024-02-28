2bfce9ed9803ae6c8dc4134bc49075ac33ab86c9
ramisha <ramishashahid22@gmail.com>
2024-02-28 19:19:07 +0500

    contactUs email

---
+++ src/app/api/email/route.js
@@ -0,0 +1,29 @@
+import { EmailTemplate } from "@/components/email/EmailTemplate";
+import { Resend } from "resend";
+
+const resend = new Resend("re_MMc2Kf8o_NG68cZhJSBj5pPcmDUj32yc5");
+import { NextResponse } from "next/server";
+
+export async function POST(req, res) {
+  try {
+    const { name, email, message, selectedOption, phoneNumber } =
+      await req.json();
+    const data = await resend.emails.send({
+      from: "forcefinancecoin<onboarding@resend.dev>",
+      to: ["ramishashahid22@gmail.com"],
+      subject: "Customer Inquiry ",
+      react: EmailTemplate({
+        name,
+        email,
+        message,
+        selectedOption,
+        phoneNumber,
+      }),
+    });
+
+    return NextResponse.json({ message: "method allow" });
+  } catch (error) {
+    console.error("Error:", error);
+    return NextResponse.json({ message: "nope" });
+  }
+}
