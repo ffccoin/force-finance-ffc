9e4401fabbee316c36bffc2370d8803502b44138
cyhammad <hammad727272@gmail.com>
2024-03-03 12:43:26 +0500

    middleware test-7

--- src/middleware.js
+++ src/middleware.js
@@ -1,4 +1,3 @@
-// pages/_middleware.ts
 import { NextRequest, NextResponse } from "next/server";
 
 export const runtime = "experimental-edge";
@@ -6,5 +5,8 @@ export const runtime = "experimental-edge";
 export function middleware(req) {
   if (req.geo.country === "CA") {
     return NextResponse.redirect("https://www.forcefinancecoin.ca/");
+  }
+  if (req.geo.country !== "CA") {
+    return NextResponse.redirect("https://www.forcefinancecoin.com/");
   }
 }
