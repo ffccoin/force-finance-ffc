6d632d08185e76599e2464197886f3f721b7f3a0
cyhammad <hammad727272@gmail.com>
2024-03-03 12:57:13 +0500

    middleware test-10

--- src/middleware.js
+++ src/middleware.js
@@ -1,10 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req) {
-  // if (req.geo.country === "CA") {
-  //   return NextResponse.redirect("https://www.forcefinancecoin.ca/");
-  // }
-  if (req.geo.country !== "CA") {
-    return NextResponse.redirect("https://www.forcefinancecoin.com/");
+  if (req.geo.country === "CA") {
+    return NextResponse.redirect("https://www.forcefinancecoin.ca/");
   }
+  // if (req.geo.country !== "CA") {
+  //   return NextResponse.redirect("https://www.forcefinancecoin.com/");
+  // }
 }
