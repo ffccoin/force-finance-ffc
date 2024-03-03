89f73376bc5e1ab4614e16a7078ae559acf22011
cyhammad <hammad727272@gmail.com>
2024-03-03 12:49:18 +0500

    middleware test-8

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 
-export const runtime = "experimental-edge";
-
 export function middleware(req) {
   if (req.geo.country === "CA") {
     return NextResponse.redirect("https://www.forcefinancecoin.ca/");
