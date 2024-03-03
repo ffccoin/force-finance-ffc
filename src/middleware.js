1edd3e7249d4e3e1ec0cf5facf0db937cdaa62a0
cyhammad <hammad727272@gmail.com>
2024-03-03 16:35:48 +0500

    update middleware 1

--- src/middleware.js
+++ src/middleware.js
@@ -1,10 +1,17 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req) {
-  if (req.geo.country === "CA") {
+  console.log("BASEPATH:", req.nextUrl.hostname);
+  if (
+    req.geo.country === "CA" &&
+    req.nextUrl.hostname === "www.forcefinancecoin.com"
+  ) {
     return NextResponse.redirect("https://www.forcefinancecoin.ca/");
   }
-  // if (req.geo.country !== "CA") {
-  //   return NextResponse.redirect("https://www.forcefinancecoin.com/");
-  // }
+  if (
+    req.geo.country !== "CA" &&
+    req.nextUrl.hostname === "www.forcefinancecoin.ca"
+  ) {
+    return NextResponse.redirect("https://www.forcefinancecoin.com/");
+  }
 }
