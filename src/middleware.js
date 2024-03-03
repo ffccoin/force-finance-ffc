50652281d8751c6a12f51093c206013d422860b9
cyhammad <hammad727272@gmail.com>
2024-03-03 12:52:22 +0500

    middleware test-9

--- src/middleware.js
+++ src/middleware.js
@@ -1,9 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(req) {
-  if (req.geo.country === "CA") {
-    return NextResponse.redirect("https://www.forcefinancecoin.ca/");
-  }
+  // if (req.geo.country === "CA") {
+  //   return NextResponse.redirect("https://www.forcefinancecoin.ca/");
+  // }
   if (req.geo.country !== "CA") {
     return NextResponse.redirect("https://www.forcefinancecoin.com/");
   }
