c48790a1565d194ec0556afdc9376bf35f59f809
cyhammad <hammad727272@gmail.com>
2024-03-03 12:37:56 +0500

    middleware test-6

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  // async redirects(){
+  //   return [
+
+  //   ]
+  // },
   images: {
     remotePatterns: [
       {
--- src/middleware.js
+++ src/middleware.js
@@ -1,15 +1,10 @@
 // pages/_middleware.ts
 import { NextRequest, NextResponse } from "next/server";
 
+export const runtime = "edge";
+
 export function middleware(req) {
-  const country = req.geo?.country || "UNKNOWN";
-  console.log("middleware", req.url);
-  if (req.url === "https://www.forcefinancecoin.ca" && country !== "CA") {
-    return NextResponse.redirect("https://www.forcefinancecoin.com", 301);
-  } else if (
-    req.url === "https://www.forcefinancecoin.com" &&
-    country === "CA"
-  ) {
-    return NextResponse.redirect("https://www.forcefinancecoin.ca", 301);
+  if (req.geo.country === "CA") {
+    return NextResponse.redirect("https://www.forcefinancecoin.ca/");
   }
 }
